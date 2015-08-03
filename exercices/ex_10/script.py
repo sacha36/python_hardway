fcf2458bbf1e2b60e2d0c08354bbef781cac8716
Sacha <sacha.lm.36@gmail.com>
2015-08-03 21:20:17 +0200

    new exercices

---
+++ exercices/ex_10/script.py
@@ -0,0 +1,7 @@
+print "hello \" i can write as mush \"\"\"\" as i want"
+print """ 
+\t das ein fat cat
+\t\t*ja?
+\t\t*nein?
+"""
+
---
+++ exercices/ex_11/script.py
@@ -0,0 +1,8 @@
+print "hello to my first I/O program in python"
+print "what's your name?"
+answer_1=raw_input()
+print "how old are you ?"
+answer_2=raw_input()
+
+print "zhalko your name is %r and you are %r old"% (answer_1, answer_2)
+
